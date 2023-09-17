1dfd5bc68a473793790c14df76ebc72a093fb092
Axel <143908475+AxelMata88@users.noreply.github.com>
2023-09-17 15:56:43 -0600

    V3 Area.cpp

--- Area.cpp
+++ Area.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-	int radio;
-    float area;
-    printf("Radio: ");
-    scanf_s("%i", &radio);
-    area = 3.1416 * (radio *radio);
-    printf("El circulo de %i de radio tiene un area de %.2f al cuadrado", radio, area);
+    int lado;
+    int area;
+    printf("Lado: ");
+    scanf_s("%i", &lado);
+    area = (lado * lado);
+    printf("El cuadrado de %i tiene un area de %i", lado, area);
 }
